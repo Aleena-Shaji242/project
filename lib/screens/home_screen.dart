d4f46f61799f19ae8a1a5e2ef9b53faffc9d84e1
Aleena-Shaji242 <aleenashaji.b23ec1212@mbcet.ac.in>
2025-01-03 14:13:18 +0530

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class practice_practice extends StatelessWidget {
+  const practice_practice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+  
+
+    );
+  }
+}
